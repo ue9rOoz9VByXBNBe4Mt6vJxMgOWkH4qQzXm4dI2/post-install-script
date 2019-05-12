45f45a24b2008e81cbe30d0c5c9eb4d19a9eff6d
Uneschewed LLC <34456023+ue9rOoz9VByXBNBe4Mt6vJxMgOWkH4qQzXm4dI2@users.noreply.github.com>
2019-05-12 01:52:44 -0500

    Create post-install-script.sh

---
+++ post-install-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#sed -i 's/#\?\(PerminRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PermitEmptyPasswords\s*\).*$/\1 no/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
