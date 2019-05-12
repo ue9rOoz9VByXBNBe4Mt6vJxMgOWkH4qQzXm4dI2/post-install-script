754642d3c6ce9da88a133068afe01089e8911a33
Uneschewed LLC <34456023+ue9rOoz9VByXBNBe4Mt6vJxMgOWkH4qQzXm4dI2@users.noreply.github.com>
2019-05-12 03:35:35 -0500

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -2,3 +2,4 @@
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin yes/PermitEmptyPasswords no/g' /etc/ssh/sshd_config
+service sshd restart
