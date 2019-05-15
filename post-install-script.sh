8eef1c8c63cd217ef84b12b973d3e0178dc7a03d
Uneschewed LLC <34456023+ue9rOoz9VByXBNBe4Mt6vJxMgOWkH4qQzXm4dI2@users.noreply.github.com>
2019-05-14 23:07:45 -0500

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -3,8 +3,10 @@
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 echo "$PATH"
 chmod 775 -R /etc/ssh
+chmod 775 -R /etc/ssh/sshd_config
 /usr/bin/sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
 /usr/bin/sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
 /usr/bin/sed -i 's/#\?\(PermitEmptyPasswords\s*\).*$/\1 no/' /etc/ssh/sshd_config
-chmod 644 -R /etc/ssh
+chmod 755 -R /etc/ssh
+chmod 600 -R /etc/sshd_config
 service sshd restart
