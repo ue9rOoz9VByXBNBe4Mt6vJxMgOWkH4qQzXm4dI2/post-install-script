3118530d7dd3fd4863762a89890e1f870174c838
Uneschewed LLC <34456023+ue9rOoz9VByXBNBe4Mt6vJxMgOWkH4qQzXm4dI2@users.noreply.github.com>
2019-05-14 23:09:45 -0500

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -8,5 +8,5 @@ chmod 775 -R /etc/ssh/sshd_config
 /usr/bin/sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
 /usr/bin/sed -i 's/#\?\(PermitEmptyPasswords\s*\).*$/\1 no/' /etc/ssh/sshd_config
 chmod 755 -R /etc/ssh
-chmod 600 -R /etc/sshd_config
+chmod 644 -R /etc/sshd_config
 service sshd restart
